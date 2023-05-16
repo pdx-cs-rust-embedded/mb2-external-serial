adae05452eb4ea71c8d60a2e5412ae835909c0b5
Bart Massey <bart.massey@gmail.com>
2023-05-15 22:38:04 -0700

    moved to v2 only for simplicity, moved UARTE0 to mb2 pins 0 and 1

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,33 @@ use panic_halt as _;
 
 use core::fmt::Write;
 
-#[cfg(feature = "v1")]
 use microbit::{
     hal::prelude::*,
-    hal::uart,
-    hal::uart::{Baudrate, Parity},
-};
-
-#[cfg(feature = "v2")]
-use microbit::{
-    hal::prelude::*,
-    hal::uarte,
-    hal::uarte::{Baudrate, Parity},
+    hal::gpio::Level,
+    hal::uarte::{self, Baudrate, Parity, Pins},
 };
 
 use cortex_m_rt::entry;
 
-#[cfg(feature = "v2")]
 mod serial_setup;
-#[cfg(feature = "v2")]
 use serial_setup::UartePort;
 
 #[entry]
 fn main() -> ! {
     let board = microbit::Board::take().unwrap();
 
-    #[cfg(feature = "v1")]
-    let mut serial = {
-        uart::Uart::new(
-            board.UART0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        )
+    let uart: Pins = Pins {
+	txd: board.pins.p0_02.into_push_pull_output(Level::High).into(),
+        rxd: board.pins.p0_03.into_floating_input().into(),
+        cts: None,
+        rts: None,
     };
 
-    #[cfg(feature = "v2")]
     let mut serial = {
         let serial = uarte::Uarte::new(
             board.UARTE0,
-            board.uart.into(),
+            // board.uart.into(),
+            uart,
             Parity::EXCLUDED,
             Baudrate::BAUD115200,
         );
